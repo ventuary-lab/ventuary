d89d9205ebd341735d199096c2bb5de2e98b3f5d
lamiqv <laumiqv@gmail.com>
2020-05-20 16:06:04 +0300

    upd index (main) page

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,33 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ventuary-site
-      </h1>
-      <h2 class="subtitle">
-        ventuary landing
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <div>
+    <HeaderBlock />
+    <First />
+    <Second />
+    <Third />
+    <Four />
+    <Five />
+    <Six />
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import HeaderBlock from "~/components/index/HeaderBlock.vue";
+import First from "~/components/index/First.vue";
+import Second from "~/components/index/Second.vue";
+import Third from "~/components/index/Third.vue";
+import Four from "~/components/index/Four.vue";
+import Five from "~/components/index/Five.vue";
+import Six from "~/components/index/Six.vue";
 
 export default {
   components: {
-    AppLogo
-  }
-}
+    HeaderBlock,
+    First,
+    Second,
+    Third,
+    Four,
+    Five,
+    Six,
+  },
+};
 </script>
-
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
-
