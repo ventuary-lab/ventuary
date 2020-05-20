196d6310ce614341bbd8809018b303a6019e4402
lamiqv <laumiqv@gmail.com>
2020-05-20 15:59:43 +0300

    upd config

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'ventuary-site',
+    title: 'ventuary',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
